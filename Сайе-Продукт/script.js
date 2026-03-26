0ef7c2ed115bdc00f7db85a096918c2b94492700
lkars <162668877+1lkars@users.noreply.github.com>
2026-03-26 23:00:04 +0800

    Delete Сайе-Продукт directory

--- Сайе-Продукт/script.js
+++
@@ -1,7 +0,0 @@
-const confirmButton = document.getElementById("confirm")
-
-function confirmscript(){
-    confirmButton.innerHTML = "Спасибо! Мы скоро с вами свяжемся."
-    confirmButton.style.backgroundColor = "#3d587e";
-    confirmButton.style.color = "#fefefe";
-}
