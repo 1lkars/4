705a4b5104a0731332651ffe79cba4fe30539fbf
lkars <162668877+1lkars@users.noreply.github.com>
2026-03-26 22:58:04 +0800

    updated

---
+++ Сайе-Продукт/script.js
@@ -0,0 +1,7 @@
+const confirmButton = document.getElementById("confirm")
+
+function confirmscript(){
+    confirmButton.innerHTML = "Спасибо! Мы скоро с вами свяжемся."
+    confirmButton.style.backgroundColor = "#3d587e";
+    confirmButton.style.color = "#fefefe";
+}
